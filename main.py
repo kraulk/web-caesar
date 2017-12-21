5dde4286f7c1b19707f0ce01486946b310ca77c1
Kristofer <kristofer@Kristofers-Air.fios-router.home>
2017-12-20 20:16:49 -0500

    Finished Web Caesar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from caesar import rotate_string
+import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -60,34 +61,6 @@ def encrypt():
 
 	return form.format(rotated_text)
 
-# Getting error that int is not iterable. Issue with converting form input for rot into something
-# that can be used in the rotated_text function.
-
-
-'''
-To process the form, define a new function encrypt in main.py. 
-Add an @app.route decorator to configure the function to receive requests at the root path "/", 
-and with methods=['POST']. ---DONE---
-
-When the form is submitted, the request will contain the parameters rot and text.
-In order to access these, we need Flask's request object. To import it,
-modify the topmost import statement to include this object. ---DONE---
-
-Within encrypt, store the values of these request parameters in local variables,
-converting data types as necessary. ---DONE---
-
-Then, encrypt the value of the text parameter using rotate_string. Return the encrypted 
-string wrapped in <h1> tags, to be rendered in the browser. ---DONE---
-
-Before embarking on our final task, start up the application and test that everything
-you've done so far works. This is also a good time to commit your changes to your local
-Git repo. ---DONE---
-
-
-
-
-
-'''
 app.run()
 
 
